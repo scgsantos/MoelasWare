2d239552c1a594470d4bfa8a4538a09603215d62
Tomás Duarte <tomasduarte@pm.me>
2022-10-20 16:37:07 +0100

    Improve time tracking script
    Add CLI parameters
    Read access token from file
    Add error handling

--- PM/time_tracking/time_tracking.py
+++ PM/time_tracking/time_tracking.py
@@ -1,52 +1,146 @@
-import requests
+"""
+Requires the `requests` and `matplotlib` modules to run.
+Install them with pip, like this: `pip install -U requests matplotlib`
+
+Write your access token to a file named `.timegraphtoken`
+
+How to use:
+    python time_tracking.py <group> <sprint>
+Example:
+    python time_tracking.py 1 3
+    (make graph for group 1's workload during sprint 3)
+"""
 import time
+import argparse
+
 import matplotlib.pyplot as plt
+import requests
 
-access_token = ""  # Enter your API key here
-project_id = 39332768
-base_url = "https://gitlab.com/api/v4"
-headers = {'Private-Token': access_token}
+PROJECT_ID = 39332768
+BASE_URL = "https://gitlab.com/api/v4"
+REQUEST_URL = f"{BASE_URL}/projects/{PROJECT_ID}/issues"
+TOKEN_FILENAME = ".timegraphtoken"
+GROUPS = [
+    {
+        "bruno.r.sebastiao": "Bruno Sebastião",
+        "OriginalNameTag": "Carlos Jordão",
+        "DuarteFonte_Santa": "Duarte Fonte-Santa",
+        "miguelfdas": "Miguel Sérgio",
+        "Nunoduarte1": "Nuno Gomes",
+        "Pedro-Cardoso7": "Pedro Cardoso",
+        "rodrigoofig": "Rodrigo Figueiredo",
+    },
+    {
+        "acolaco30": "André Colaço",
+        "DanielaCosta": "Daniela Costa",
+        "GamingBound": "João Leite",
+        "larasilveiraluz2021": "Lara Luz",
+        "sofiaTrindade": "Sofia Trindade",
+        "tomasduarte": "Tomás Duarte",
+    },
+    {
+        "CarlosMOMatos": "Carlos Matos",
+        "iTzHuGo": "Hugo Barros",
+        "mmagueijo": "Mariana Magueijo",
+        "ertem0": "Vasco Goveia",
+        "aritaoliveira": "Rita Oliveira",
+        "joaopino": "João Pino",
+    },
+    {
+        "Elenanni": "Elena Barrera",
+        "loboh67": "Henrique Lobo",
+        "josessilva28": "José Silva",
+        "LeomPina": "Leonardo Pina",
+        "ThunderStorm710": "Pedro Ascensão",
+        "rafa_74": "Rafael Ferreira",
+        "scgsantos": "Sofia Santos",
+    },
+    {
+        "af0onso_duarte": "Afonso Duarte",
+        "itsdanielalopes": "Daniela Lopes",
+        "PolpEdu": "Eduardo Nunes",
+        "GuilhermeFFaria": "Guilherme Faria",
+        "mariislvs": "Mariana Silva",
+        "miguelopesantana": "Miguel Santana",
+    },
+]
 
 
-def time_tracking(username, milestone):
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Grab time tracking info from GitLab and plot it"
+    )
+    parser.add_argument("group", type=int, help="The target group number")
+    parser.add_argument("sprint", type=int, help="The target sprint number")
+
+    return parser.parse_args()
+
+
+def read_gitlab_token():
+    try:
+        with open(TOKEN_FILENAME) as fp:
+            return fp.read().strip()
+    except FileNotFoundError:
+        print(
+            f"Couldn't find {TOKEN_FILENAME} file! "
+            "Make sure it exists and contains your access token."
+        )
+        exit(1)
+
+
+def time_tracking(token, user, milestone):
+    print(f"Requesting info for {user[1]} ({user[0]})")
     total_time = 0
+    request = requests.get(
+        REQUEST_URL,
+        headers={"Private-Token": token},
+        params={"assignee_username": user[0], "milestone": milestone},
+    )
+    request.raise_for_status()
 
-    request_url = f"{base_url}/projects/{project_id}/issues?assignee_username={username}&milestone={milestone}"
-
-    project_request = requests.get(request_url, headers=headers)
-
-    issues = {issue["title"]: issue["time_stats"] for issue in project_request.json()}
+    issues = {issue["title"]: issue["time_stats"] for issue in request.json()}
     time_values = issues.values()
     for v in time_values:
-        total_time += v.get('total_time_spent')
+        total_time += v.get("total_time_spent")
 
     # Convert to hours
     time_aux = time.gmtime(total_time)
-    time_num = time_aux.tm_hour + ((time_aux.tm_min*100)/60)/100
+    time_num = time_aux.tm_hour + time_aux.tm_min / 60
     return time.strftime("%H:%M:%S", time_aux), time_num
 
 
-if __name__ == "__main__":
-    milestone = "Sprint 3"
-    group_2 = ['acolaco30', 'DanielaCosta', 'GamingBound', 'larasilveiraluz2021', 'sofiaTrindade', 'tomasduarte']
-    names_n = ['André\nColaço', 'Daniela\nCosta', 'João\nLeite', 'Lara\nLuz', 'Sofia\nTrindade', 'Tomás\nDuarte']
-    names = ['André Colaço', 'Daniela Costa', 'João Leite', 'Lara Luz', 'Sofia Trindade', 'Tomás Duarte']
+def run():
+    args = parse_args()
+    token = read_gitlab_token()
+
+    milestone = f"Sprint {args.sprint}"
+    group = GROUPS[args.group - 1]
     total_time_list = []
     results = []
 
-    for i in range(len(group_2)):
-        time_str, time_num = time_tracking(group_2[i], milestone)
+    for user in group.items():
+        time_str, time_num = time_tracking(token, user, milestone)
         total_time_list.append(time_num)
         results.append(time_str)
 
+    print("Building graph")
     fig = plt.figure(figsize=(10, 5))
 
     plt.figure(1)
-    plt.bar(names_n, total_time_list)
-    plt.ylabel('Total Time (Hours)')
-    plt.title('Time Tracking - ' + milestone + ' - Group 2')
-    for i in range(len(group_2)):
+    plt.bar([n.replace(" ", "\n") for n in group.values()], total_time_list)
+
+    plt.ylabel("Total Time (Hours)")
+    plt.title(f"Time Tracking - {milestone} - Group {args.group}")
+    for i, _ in enumerate(group):
         plt.text(i - 0.25, 2, results[i])
 
-    fig.savefig('Time Tracking - ' + milestone + ' - Group 2.svg', bbox_inches='tight', dpi=150)
+    fig.savefig(
+        f"Time Tracking - {milestone} - Group {args.group}.svg",
+        bbox_inches="tight",
+        dpi=150,
+    )
     plt.show()
+
+
+if __name__ == "__main__":
+    run()
