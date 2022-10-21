2dc41945f7ed90ff4c4c01ace16852d743e78271
Rodrigo Santos Figueiredo <rodres2002@gmail.com>
2022-10-21 21:28:00 +0100

    Add my frontend code for unfinished quizzes

--- dev/frontend/UnfinishedQuizzes/src/App.js
+++ dev/frontend/UnfinishedQuizzes/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <div class = "logo" style={{ flex: 2}}>
           <img src={logo} alt="logo" />
         </div>
-        
+
         <div class = "username" style={{ flex: 4}}>
           <text style={styles.geral}>Hi, username</text>
         </div>
@@ -20,10 +20,10 @@ function App() {
           <text style = {styles.middletitle}>LIST OF UNFINISHED QUIZZES</text>
         </div>
       </div>
-      
+
       <div class = 'columns'>
         <div class = 'middlecolumn'>
-          
+
             <div style={{flex: 1}}>
               <Button buttonNumber = {1}/>
               <Button buttonNumber = {2}/>
@@ -34,12 +34,12 @@ function App() {
               <Button buttonNumber = {7}/>
               <Button buttonNumber = {8}/>
             </div>
-            
-          
+
+
         </div>
-      
+
         <div class = 'leftcolumn'>
-          
+
             <div style={{flex: 1}}>
               <Button buttonNumber = {9}/>
               <Button buttonNumber = {10}/>
@@ -50,11 +50,11 @@ function App() {
               <Button buttonNumber = {15}/>
               <Button buttonNumber = {16}/>
             </div>
-            
-          
+
+
         </div>
         <div class = 'rightcolumn'>
-         
+
             <div style={{flex: 1}}>
               <Button buttonNumber = {17}/>
               <Button buttonNumber = {18}/>
@@ -65,7 +65,7 @@ function App() {
               <Button buttonNumber = {23}/>
               <Button buttonNumber = {24}/>
             </div>
-          
+
         </div>
       </div>
     </div>
--- dev/frontend/UnfinishedQuizzes/src/components/buttons.jsx
+++ dev/frontend/UnfinishedQuizzes/src/components/buttons.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import "../CSS/index.css";
 
 class Counter extends Component {
-    
+
     render() { 
         return (
             <div class = 'unfinishedquiz'>
