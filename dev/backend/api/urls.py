0c90b8d4304c9d230e247951c781c630025844af
GuiFFaria <guifaria02@gmail.com>
2022-10-22 02:41:58 +0100

    preconditions

--- dev/backend/api/urls.py
+++ dev/backend/api/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
 	path('tests/<int:pk>/', views.get_test_view),
+	path('tests/', views.create_test_view),
+	path('select_test/<int:pk>/', views.select_test_view),
 ]
 
--- dev/backend/api/views.py
+++ dev/backend/api/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import get_object_or_404
 
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-from moelasware.models import Test
+from moelasware.models import Quiz, Test
 from .serializers import GetTestSerializer, CreateTestSerializer
 
 
@@ -12,7 +14,7 @@ def get_test_view( request, pk, *args, **kwargs ):
 
 	# get test by id -> detail view
 	instance = get_object_or_404(Test, pk=pk)
-	serializer = GetTestSerializer(obj, many=False)
+	serializer = GetTestSerializer(instance, many=False)
 
 	return JsonResponse({'test': serializer.data})
 
@@ -29,3 +31,35 @@ def create_test_view( request ):
 		return Response(serializer.data)
 
 	return Response({'invalid': 'not good data'}, status=400)
+
+@api_view(['GET'])
+# function that selects the test that the user wants to solve
+def select_test_view(request, pk):
+	# verify if the user has created at least one test
+	done_quiz = Quiz.objects.filter(author=request.user.username)
+	# verify if there is at least one test available to solve
+	available_test = Test.objects.all()
+	if request.method == 'GET':
+	#verify if the user is logged in
+		if request.user.is_authenticated:
+			if done_quiz.exists():
+				if available_test.exists():
+					# get the test by id
+					object = Test.objects.filter(Test, pk=pk)
+					serializer = GetTestSerializer(object, many=False)
+					return JsonResponse({'test': serializer.data})
+				else:
+					data = {
+						'error' : 'There are no tests available to solve'
+					}
+					return JsonResponse(data, status = status.HTTP_404_NOT_FOUND)
+			else:
+				data = {
+					'error' : 'You have not created any test yet'
+				}
+				return JsonResponse(data, status = status.HTTP_412_PRECONDITION_FAILED)
+		else:
+			data = {
+				'error' : 'You are not logged in'
+			}
+			return JsonResponse(data, status = status.HTTP_401_UNAUTHORIZED)
