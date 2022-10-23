2ae33df44dfa6496e4715fa89dbe7787370a8479
Polpy <emailparaoedu@gmail.com>
2022-10-23 12:30:23 +0100

    fixing Issue Threads from MR #151

--- dev/backend/api/urls.py
+++ dev/backend/api/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('tests/<int:pk>/', views.get_test_view),
-	path('tests/', views.create_test_view),
-	path('/tests/<int:pk>/submissions', views.submission_view),
+    path('tests/<int:pk>', views.get_test_view),
+    path('tests', views.create_view),
+    path('tests/<int:pk>/submissions', views.submission_view),
 ]
-
