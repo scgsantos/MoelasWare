20797150d0b2c7668883f063e860784988b80638
Sofia Trindade <sofiacaetanotrindade@gmail.com>
2022-10-23 22:55:40 +0000

    Create endpoint: get all tests

--- dev/backend/api/urls.py
+++ dev/backend/api/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path("tests/<int:pk>/", views.get_test_view),
     path("tests/<int:pk>/submissions/", views.submission_of_a_test_view),
-    path("tests/", views.get_all_tests_view),
+    path("tests/", views.tests_view),
     path("users/<int:pk>/submissions/", views.submissions_by_user_view),
     path("fame/", views.hall_of_fame_view),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
