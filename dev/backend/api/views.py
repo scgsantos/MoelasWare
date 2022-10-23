194c4bbc36648fca2fc947a202a67ed471b5b5af
Tomás Duarte <tomasduarte@pm.me>
2022-10-24 00:12:59 +0100

    Improve create test endpoint code
    Closes #245

--- dev/backend/api/views.py
+++ dev/backend/api/views.py
@@ -32,55 +32,44 @@ def post_test_view(request):
     # TODO: ADD THIS LINE WHEN LOGIN IS IMPLEMENTED
     # author_id = request.user.id
     author_id = 1
-    name = request.data.get('name')
-
-    deserializer_data = {'author': author_id, 'name': name}
 
     if 'quizzes' not in request.data.keys() and not {'num_quizzes', 'allowed_tags'} <= request.data.keys():
-            return HttpResponseBadRequest(
-                    "You must provide either the number of quizzes and allowed tags, or the quizzes")
+        return HttpResponseBadRequest(
+            "You must provide either the number of quizzes and allowed tags, or the quizzes"
+        )
 
     try:
         num_quizzes = int(request.data.get('num_quizzes'))
     except ValueError:
-            return HttpResponseBadRequest(f"The number of quizzes must be a number and not {request.data.get('num_quizzes')}")
+        return HttpResponseBadRequest(
+            f"The number of quizzes must be a number and not {request.data.get('num_quizzes')}"
+        )
                     
-    if 'quizzes' not in request.data.keys():
-        tags = request.data.get("allowed_tags")
-        if tags:
-            quizzes_set = Quiz.objects.order_by("?").filter(tags__text__in=tags).distinct()[:num_quizzes]
-        else:
-            # any quizz with any tag(s) can be randomly chosen
-            quizzes_set = Quiz.objects.order_by("?").all()[:num_quizzes]
-
-        # Not enough quizzes that meet the specs
-        if len(quizzes_set) < num_quizzes:
-            return HttpResponseBadRequest(
-                    "The number of requested quizzes is bigger than the number of existing quizzes meeting the given specifications")
-
-
-        deserializer_data.update({'quizzes': quizzes_set.values_list('id', flat=True)})
-
+    def respond(quizzes):
+        name = request.data.get('name')
+        deserializer_data = {'author': author_id, 'name': name, 'quizzes': quizzes}
         test_deserializer = CreateTestSerializer(data=deserializer_data)
         if test_deserializer.is_valid(raise_exception=True): 
             test = test_deserializer.save()
-
             response_serializer = GetTestSerializer(test)
             return JsonResponse({'test': response_serializer.data})
-
 
     # Required data was all in request (quizzes list was given)
-    else:
-        deserializer_data.update({'quizzes': request.data.get('quizzes')})
-        test_deserializer = CreateTestSerializer(data=deserializer_data)
+    if 'quizzes' in request.data.keys():
+        return respond(request.data.get('quizzes'))
 
-        if test_deserializer.is_valid(raise_exception=True): 
-            test = test_deserializer.save() # save test instance in the db
-            response_serializer = GetTestSerializer(test)
-            return JsonResponse({'test': response_serializer.data})
+    tags = request.data.get("allowed_tags")
+    quizzes_set = Quiz.objects.order_by("?")
+    quizzes_set = quizzes_set.all() if tags else quizzes_set.filter(tags__text__in=tags).distinct()
+    quizzes_set = quizzes_set[:num_quizzes]
 
+    # Not enough quizzes that meet the specs
+    if len(quizzes_set) < num_quizzes:
+        return HttpResponseBadRequest(
+            "The number of requested quizzes is bigger than the number of existing quizzes meeting the given specifications"
+        )
 
-    return JsonResponse({'invalid': 'not good data'}, status=400)
+    return respond(quizzes_set.values_list('id', flat=True))
 
 
 # @api_view(['GET'])
