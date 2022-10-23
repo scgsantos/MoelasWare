6be1e07466e2c7ccda20a2671c6bd627ce180f10
ertem0 <35893360+ertem0@users.noreply.github.com>
2022-10-23 23:19:23 +0100

    page and logo

---
+++ dev/frontend/src/pages/ReviewQuizPage.js
@@ -0,0 +1,17 @@
+import './ReviewQuizPage.css';
+import React from 'react';
+import Logo from '../components/Logo';
+function ReviewQuizPage() {
+    return (
+        <div style={{
+            backgroundColor: '#f5f5f5',
+            flex:1,
+
+
+        }}>
+            <Logo></Logo>
+        </div>
+    );
+  }
+  
+export default ReviewQuizPage;
