5ab9aa4fcd78e9c8400d21505e167f4b82da3c4e
Daniela Costa <daniela.costa2002@gmail.com>
2022-10-24 10:59:24 +0000

    Add endpoint to get all tags or a specific tag by id

--- dev/backend/api/urls.py
+++ dev/backend/api/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("tests/<int:pk>/", views.get_test_view),
     path("tests/<int:pk>/submissions/", views.submission_of_a_test_view),
     path("tests/", views.tests_view),
+    path("tags/<int:pk>/", views.get_tag_view),
+    path("tags/", views.get_tag_view),
     path("users/<int:pk>/submissions/", views.submissions_by_user_view),
     path("fame/", views.hall_of_fame_view),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
