e377bd131424e4f30d271e3c520f751c043d3a08
ertem0 <35893360+ertem0@users.noreply.github.com>
2022-10-24 01:07:18 +0100

    changes

--- dev/frontend/src/pages/ReviewQuizPage.js
+++ dev/frontend/src/pages/ReviewQuizPage.js
@@ -1,16 +1,37 @@
-import Container from './ReviewQuizPage.css';
+import './ReviewQuizPage.css';
 import React from 'react';
 import Logo from '../components/Logo';
 
 function ReviewQuizPage() {
     return (
-        <body className='Container'>
-            <div>
-                <Logo />
-            </div>
-        </body>
+    <div class="Container">
+      <div class = "topbar">
+        <div style={{ flex: 1}}></div>
+        <div class = "logo" style={{ flex: 2}}>
+          <Logo/>
+        </div>
+        
+        <div class = "username" style={{ flex: 4}}>
+          <text style={styles.geral}>Hi, username</text>
+        </div>
+        <div>
+          <text style = {styles.middletitle}>LIST OF UNFINISHED QUIZZES</text>
+        </div>
+      </div>
+</div>
     );
   }
 
+  const styles = {
+    geral: {
+      fontSize: 20,
+    },
+    middletitle:{
+      fontSize: 20,
+      fontWeight: "bold"
+    },
+  
+  };
+  
   
 export default ReviewQuizPage;
