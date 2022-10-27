3e71b843bc3cb8189a3a4e415b56ca0209d54820
Duarte Fonte-Santa <uc2018274628@student.uc.pt>
2022-10-27 14:04:38 +0000

    Update views.py

--- dev/backend/api/views.py
+++ dev/backend/api/views.py
@@ -3,29 +3,23 @@ from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
-from moelasware.models import Test
-from .serializers import GetTestSerializer, CreateTestSerializer
+from moelasware.models import Quiz
+from .serializers import CreateQuizSerializer, GetQuizSerializer
 
 
-@api_view(['GET']) # allowed method(s)
-def get_test_view( request, pk, *args, **kwargs ):
-
-	# get test by id -> detail view
-	instance = get_object_or_404(Test, pk=pk)
-	serializer = GetTestSerializer(obj, many=False)
-
-	return JsonResponse({'test': serializer.data})
-
-
-
+@api_view(['GET'])
+def get_quiz_view(request,pk, *args, **kwargs):
+	instance = get_object_or_404(Quiz, pk=pk)
+	serializer = GetQuizSerializer(obj, many=False)
+	
+	return JsonResponse({'quiz' : serializer.data})
+	
+	
 @api_view(['POST'])
-def create_test_view( request ):
-	serializer = CreateTestSerializer(data=request.data)
-
-	if serializer.is_valid(raise_exception=True): # raises exception on why its not valid
-		#instance = serializer.save()
-		
-		#print(instance)
+def create_quiz_view(request):
+	serializer = CreateQuizSerializer(data = request.data)
+	
+	if serializer.is_valid(raise_exception=True):
 		return Response(serializer.data)
-
+		
 	return Response({'invalid': 'not good data'}, status=400)
