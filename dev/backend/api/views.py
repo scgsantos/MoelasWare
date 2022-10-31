075b1b60d8dd5c820c937452fb41af7a338eb12d
GaytisticDisaster <daniela.costa2002@gmail.com>
2022-10-31 12:48:34 +0000

    fix endpoint to get n quizzes from given specs

--- dev/backend/api/views.py
+++ dev/backend/api/views.py
@@ -12,13 +12,14 @@ from api.serializers import CreateTestSerializer, GetTestSerializer, QuizSeriali
 DEFAULT_TEST_PAGE_LIMIT = 20
 DEFAULT_TAG_PAGE_LIMIT = 20
 
-@api_view(['GET']) 
-def get_test_view(request, pk, *args, **kwargs):
-	# get test by id -> detail view
-	instance = get_object_or_404(Test, pk=pk)
-	serializer = GetTestSerializer(instance, many=False)
 
-    return JsonResponse({'test': serializer.data})
+@api_view(["GET"])
+def get_test_view(request, pk, *args, **kwargs):
+    # get test by id -> detail view
+    instance = get_object_or_404(Test, pk=pk)
+    serializer = GetTestSerializer(instance, many=False)
+
+    return JsonResponse({"test": serializer.data})
 
     return JsonResponse({'test': serializer.data})
 
@@ -31,42 +32,51 @@ def post_test_view(request):
     # author_id = request.user.id
     author_id = 1
 
-    quizzes = get_n_quizzes_view(request)
+    # Required data was all in request (quizzes list was given)
+    if "quizzes" in request.data.keys():
+        quizzes = request.data.get("quizzes")
 
-    name = request.data.get('name')
-    deserializer_data = {'author': author_id, 'name': name, 'quizzes': quizzes}
+    else:
+        quizzes_set = get_n_quizzes_view(request)["quizzes"]
+        quizzes = quizzes_set.values_list("id", flat=True)
+
+    name = request.data.get("name")
+    deserializer_data = {"author": author_id, "name": name, "quizzes": quizzes}
     test_deserializer = CreateTestSerializer(data=deserializer_data)
-    if test_deserializer.is_valid(raise_exception=True): 
+
+    if test_deserializer.is_valid(raise_exception=True):
         test = test_deserializer.save()
         response_serializer = GetTestSerializer(test)
-        return JsonResponse({'test': response_serializer.data})
+        return JsonResponse({"test": response_serializer.data})
 
-@api_view(['GET']) 
+
+@api_view(["GET"])
 def get_n_quizzes_view(request):
     # If no quizzes are sent in the request -> quizzes are selected randomly
     # User gave config (num_quizes, allowed_tags (optional))
 
-    if 'quizzes' not in request.data.keys() and not {'num_quizzes', 'allowed_tags'} <= request.data.keys():
+    if not "num_quizzes" in request.data.keys():
         return HttpResponseBadRequest(
-            "You must provide either the number of quizzes and allowed tags, or the quizzes"
+            "You must provide the number of quizzes"
         )
 
     try:
-        num_quizzes = int(request.data.get('num_quizzes'))
+        num_quizzes = int(request.data.get("num_quizzes"))
     except ValueError:
         return HttpResponseBadRequest(
             f"The number of quizzes must be a number and not {request.data.get('num_quizzes')}"
         )
-                
-
-    # Required data was all in request (quizzes list was given)
-    if 'quizzes' in request.data.keys():
-        return respond(request.data.get('quizzes'))
 
     tags = request.data.get("allowed_tags")
     quizzes_set = Quiz.objects.order_by("?")
-    quizzes_set = quizzes_set.all() if tags else quizzes_set.filter(tags__text__in=tags).distinct()
+
+    quizzes_set = (
+        quizzes_set.all() if not tags
+        else quizzes_set.filter(tags__text__in=tags).distinct()
+    )
+
     quizzes_set = quizzes_set[:num_quizzes]
+
 
     # Not enough quizzes that meet the specs
     if len(quizzes_set) < num_quizzes:
@@ -74,58 +84,59 @@ def get_n_quizzes_view(request):
             "The number of requested quizzes is bigger than the number of existing quizzes meeting the given specifications"
         )
 
-    return quizzes_set.values_list('id', flat=True)
+    quizzes_serializer = QuizSerializer( quizzes_set, many=True)
+
+    return JsonResponse({"quizzes": quizzes_serializer.data})
 
 
 # @api_view(['GET'])
 def get_all_tests_view(request):
     try:
-        offset = int(request.query_params.get('offset', default=0))
-        limit = int(request.query_params.get('limit', default=DEFAULT_TEST_PAGE_LIMIT))
+        offset = int(request.query_params.get("offset", default=0))
+        limit = int(request.query_params.get("limit", default=DEFAULT_TEST_PAGE_LIMIT))
     except ValueError:
         return HttpResponseBadRequest("Invalid offset and/or limit")
 
     # TODO: think about actually returning +1 records, for simplifying "Next"-type buttons on frontend
-    tests = Test.objects.filter(pk__range=(offset, offset+limit-1))
+    tests = Test.objects.filter(pk__range=(offset, offset + limit - 1))
 
     serializer = GetTestSerializer(tests, many=True)
-    return JsonResponse({'tests': serializer.data})
+    return JsonResponse({"tests": serializer.data})
 
 
 # HIGHLY TEMPORARY SOLUTION!
 # TODO: move these to class based views once overall implementation is in better shape
 # ~tomasduarte
-@api_view(['GET', 'POST'])
+@api_view(["GET", "POST"])
 def tests_view(request):
     proxy = {
-        'GET': get_all_tests_view,
-        'POST': post_test_view,
+        "GET": get_all_tests_view,
+        "POST": post_test_view,
     }
     return proxy[request.method](request)
-	return Response({'invalid': 'not good data'}, status=400)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def get_tag_view(request, pk, *args, **kwargs):
 
-	if pk is not None:
-		instance = get_object_or_404(Tag, pk=pk)
-		serializer = GetTagSerializer(instance, many=False)
+    if pk is not None:
+        instance = get_object_or_404(Tag, pk=pk)
+        serializer = GetTagSerializer(instance, many=False)
 
-		return JsonResponse({'tag': serializer.data})
+        return JsonResponse({"tag": serializer.data})
 
-	return Response({'invalid': 'not good data'}, status=400)
+    return Response({"invalid": "not good data"}, status=400)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def get_all_tags_view(request, *args, **kwargs):
-	try:
-		offset = int(request.query_params.get('offset', default=0))
-		limit = int(request.query_params.get('limit', default=DEFAULT_TAG_PAGE_LIMIT))
-	except ValueError:
-		return HttpResponseBadRequest("Invalid offset and/or limit")
+    try:
+        offset = int(request.query_params.get("offset", default=0))
+        limit = int(request.query_params.get("limit", default=DEFAULT_TAG_PAGE_LIMIT))
+    except ValueError:
+        return HttpResponseBadRequest("Invalid offset and/or limit")
 
-	queryset = Tag.objects.filter(pk__range=(offset, offset+limit))
+    queryset = Tag.objects.filter(pk__range=(offset, offset + limit))
 
-	serializer = GetTagSerializer( queryset, many=True)
-	return JsonResponse({'tags': serializer.data})
+    serializer = GetTagSerializer(queryset, many=True)
+    return JsonResponse({"tags": serializer.data})
