33336faef017df367805c00611f30e7ed2176e23
Carlos Jordão <cecjordao@gmail.com>
2022-11-01 15:34:57 +0000

    Update views.py

--- dev/backend/api/views.py
+++ dev/backend/api/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import get_object_or_404
+
+from pickle import OBJ
+from django.shortcuts import get_object_or_404, render, redirect
 
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from moelasware.models import Test
-from .serializers import GetTestSerializer, CreateTestSerializer
+from .serializers import GetTestSerializer, CreateTestSerializer, Login, LoginSerializer, Registration, RegsitrationSerializer
+
+from django.contrib.auth.models import User as AuthUser
 
 
 @api_view(['GET']) # allowed method(s)
@@ -12,7 +17,7 @@ def get_test_view( request, pk, *args, **kwargs ):
 
 	# get test by id -> detail view
 	instance = get_object_or_404(Test, pk=pk)
-	serializer = GetTestSerializer(obj, many=False)
+	serializer = GetTestSerializer(OBJ, many=False)
 
 	return JsonResponse({'test': serializer.data})
 
@@ -29,3 +34,44 @@ def create_test_view( request ):
 		return Response(serializer.data)
 
 	return Response({'invalid': 'not good data'}, status=400)
+
+
+@api_view(['POST'])
+def login( request ):
+	
+	login = LoginSerializer(data=request.data)
+
+	##probably need to change Response to JsonResponse 
+	if login.is_valid(raise_exception=True):
+		
+		if (AuthUser.objects.filter(username = login.data["username"]).exists()):
+			
+			user = AuthUser.objects.get(username = login.data["username"])
+			
+			if(user.check_password(login.data["password"])):
+				##need token? just sending id is kinda useless no?
+				return JsonResponse({'successfull' : user.id}, status=200)
+			else:
+				return JsonResponse({'invalid' : 'Wrong Credentials'}, status=400)
+		else:	
+			return JsonResponse({'invalid': 'Wrong Credentials'}, status=400)		
+	else:
+		return JsonResponse({'invalid' : 'Missing Credentials'}, status=400)
+
+	
+@api_view(['POST'])
+def register( request ):
+	
+	registration = RegsitrationSerializer(data=request.data)
+
+	if registration.is_valid(raise_exception=True):
+		if(AuthUser.objects.filter(email = registration.data["email"]).exists() or AuthUser.objects.filter(username = registration.data["username"]).exists()):
+			return JsonResponse({'invalid' : "Cannot Register"}, status=400)
+		else:
+			if(registration.data["password1"] != registration.data["password2"]):
+				return JsonResponse({'invalid' : "Passwords do not match"}, status=400)
+			AuthUser.objects.create_user(registration.data["username"], registration.data["email"], registration.data["password1"])
+
+			return JsonResponse({'success' : 'Sucessfull Registered'}, status=200)
+	else:
+		return JsonResponse({'invalid' : 'Missing Credentials'}, status=400)
