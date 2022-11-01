096c8f32c40cb60c928274d4081bf19d656d8584
João Pedro Leite <gamingbound@gmail.com>
2022-11-01 09:54:05 +0000

    Add JWT token endpoints with djangorestframework-simplejwt.

--- dev/backend/api/urls.py
+++ dev/backend/api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
 from . import views
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
 	path('tests/<int:pk>/', views.get_test_view),
 	path('tests/', views.tests_view),
     path('tags/<int:pk>/', views.get_tag_view),
 	path('tags/', views.get_tag_view),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
