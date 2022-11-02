f179f70f5ec64b6861f47f5a5cc6cdf4a35078c8
GuiFFaria <guifaria02@gmail.com>
2022-11-02 19:35:00 +0000

    optimize the code of "verifying preconditions"

--- dev/backend/api/views.py
+++ dev/backend/api/views.py
@@ -39,31 +39,19 @@ def submission_view(request, pk):
 	# verify if the user has created at least one test
 	done_quiz = Quiz.objects.filter(author=request.user.pk)
 	
-	# verify if there is at least one test available to solve
-	available_test = Test.objects.first()
-	
 	# verify if the user is not logged in
 	if not request.user.is_authenticated:
 		return JsonResponse({'error': 'You must be logged in to solve a test'}, status = status.HTTP_401_UNAUTHORIZED)
-	elif not done_quiz.exists():
+
+	if not done_quiz.exists():
 		return JsonResponse({'error': 'You must create at least one quiz to solve a test'}, status = status.HTTP_412_PRECONDITION_FAILED)
-	elif not available_test.exists():
+
+	# verify if there is at least one test available to solve
+	if not Test.objects.exists():
 		return JsonResponse({'error': 'There are no tests available to solve'}, status = status.HTTP_404_NOT_FOUND)
-	else:
-		# get the test that the user wants to solve
-		test = get_object_or_404(Test, pk=pk)
-		serializer = GetTestSerializer(test, many=False)
-		
-		return JsonResponse({'test': serializer.data})
-
-
-
-
-
-
-
-
-
-
-
 	
+	# get the test that the user wants to solve
+	test = get_object_or_404(Test, pk=pk)
+	serializer = GetTestSerializer(test, many=False)
+	
+	return JsonResponse({'test': serializer.data})
