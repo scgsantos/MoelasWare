ab0d5eb33985d8d3f3246f0b85498d8fa7e1e84d
aritaoliveira <anarita19oliveira@gmail.com>
2022-11-02 15:10:02 +0000

    Update dev/frontend/src/pages/login.css, dev/frontend/src/pages/login.js, dev/frontend/src/App.js

---
+++ dev/frontend/src/pages/login.js
@@ -0,0 +1,38 @@
+import './login.css';
+import logo from '../logo.svg';
+
+function Login() {
+  return (
+    <div>
+      <div className="start">
+        <div className="click">
+          <img src={logo} alt="Logo Moelas Ware" />
+        </div>
+        <div className="click">
+          <h1>Login/Register</h1>
+        </div>
+      </div>
+      <div className="loc">
+        <div className="login">
+          <h2>
+            LOGIN
+          </h2>
+          <input id="username_log" type="username" placeholder='username'></input>
+          <input id="password_log" type="password" placeholder='password'></input>
+          <button type="enter_log" onclick="#login">ENTER</button>
+        </div>
+        <div className="register">
+          <h2>
+            REGISTER
+          </h2>
+          <input id="username_reg" type="username" placeholder='username'></input>
+          <input id="password_reg" type="password" placeholder='password'></input>
+          <input id="repeat_password" type="password" placeholder='repeat password'></input>
+          <button type="enter_reg" onclick="#register">ENTER</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
