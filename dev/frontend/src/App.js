da30545a04b224b960797bb2d4524c56661319ac
GaytisticDisaster <daniela.costa2002@gmail.com>
2022-11-04 16:27:19 +0000

    basis for connecting components

--- dev/frontend/src/App.js
+++ dev/frontend/src/App.js
@@ -1,11 +1,23 @@
+import React from 'react';
 import CreateRandomTest from "./pages/req_2_1";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import history from './history';
+
 import "./common.css";
 
-import QuizList from "./QuizList.js";
 
-function App() {
-  return <QuizList/>
+export default class App extends React.Component() {
+  render() {
+    return (
+      <div className="App">
+        <h1> Home Page </h1>
+
+        <form>
+            <button  onClick={() => history.push('/RandomTest')}>Random Test</button>
+          </form>
+      </div>
+    );
+  }
 }
 
-export default App;
