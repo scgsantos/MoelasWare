302c5b45f0d69301a49a08ba39a8157659697e6f
Daniela Costa <daniela.costa2002@gmail.com>
2022-11-04 14:56:48 +0000

    Create Test Preview Frontend

--- dev/frontend/src/index.js
+++ dev/frontend/src/index.js
@@ -6,8 +6,12 @@ import "./common.css";
 import App from "./App";
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 
 // If you want to start measuring performance in your app, pass a function
