58edfc0f18457157f1c2e863b5dccb43d5c0eb2a
João Pedro Leite <gamingbound@gmail.com>
2022-11-06 15:26:28 +0000

    Change directory structure and add absolute imports

--- dev/frontend/src/index.js
+++ dev/frontend/src/index.js
@@ -9,7 +9,6 @@ import App from "App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
