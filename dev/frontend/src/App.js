9e63951b3a75ea70344b8813e2cc2ceabffaed82
Bruno Sebastião - 2019217538 <bruno.r.sebastiao@gmail.com>
2022-11-06 22:36:29 +0000

    Upload New File

---
+++ dev/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainCreateQuiz from "./components/mainCreateQuiz";
+
+const App = () => {
+  return (
+      <MainCreateQuiz/>
+  );
+};
+
+
+
+export default App;
