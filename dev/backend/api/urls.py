53d46433a43c1cd690ba436670c29f2a3027a518
Carlos Jordão <cecjordao@gmail.com>
2022-11-07 00:28:00 +0000

    Update urls.py

--- dev/backend/api/urls.py
+++ dev/backend/api/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 	path('tests/<int:pk>/', views.get_test_view),
 	path('/quizzes/{quiz_id}',views.createquiz),
 	path('login/', views.login),
-    path('register/', views.register),
+	path('register/', views.register),
 ]
 
