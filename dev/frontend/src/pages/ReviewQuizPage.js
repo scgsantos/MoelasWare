849bb4435143e7877c7fcde4292e84c489125e07
aritaoliveira <102245591+aritaoliveira@users.noreply.github.com>
2022-11-07 02:15:46 +0000

    Requisito_3.1

--- dev/frontend/src/pages/ReviewQuizPage.js
+++ dev/frontend/src/pages/ReviewQuizPage.js
@@ -1,60 +1,86 @@
-import './ReviewQuizPage.css';
-import React from 'react';
-import Logo from '../components/Logo';
-import { useNavigate } from "react-router-dom";
-
+import './ReviewAQuizPage.css';
+import logo from '../logo.svg';
 
 function ReviewQuizPage() {
-  let navigate = useNavigate();
-  let data = [
-    {
-      "id": 1,
-      "name": "Quiz 1",
-      "tag": "tag1",
-      "creator": "creator1",
-      "creation_date": "2021-05-01",
-      "reviews": 1,
-    },
-    {
-      "id": 2,
-      "name": "Quiz 2",
-      "tag": "tag2",
-      "creator": "creator2",
-      "creation_date": "2021-05-02",
-      "reviews": 2,
-    },
-  ]
-  
-  return (
-    <div>
-        <div class="background"></div>
-      <div class = "topbar">
-        <div style={{ flex: 1}}></div>
-        <div class = "logo" style={{ flex: 2}}>
-          <Logo/>
+  const data = [["Quiz #1 Name", "Tag", "Name of creater", "Creation date", "X/3 reviews"],
+                ["Quiz #2 Name", "Tag", "Name of creater", "Creation date", "X/3 reviews"],
+                ["Quiz #3 Name", "Tag", "Name of creater", "Creation date", "X/3 reviews"],
+                ["Quiz #4 Name", "Tag", "Name of creater", "Creation date", "X/3 reviews"],
+                ["Quiz #5 Name", "Tag", "Name of creater", "Creation date", "X/3 reviews"],
+                ["Quiz #6 Name", "Tag", "Name of creater", "Creation date", "X/3 reviews"],
+                ["Quiz #7 Name", "Tag", "Name of creater", "Creation date", "X/3 reviews"],
+                ["Quiz #8 Name", "Tag", "Name of creater", "Creation date", "X/3 reviews"]];
+    return (
+      <div>
+        <div className="start">
+          <div className="click">
+            <img src={logo} alt="Logo Moelas Ware" />
+          </div>
+          <div className="click">
+            <h1>Hi, username</h1>
+          </div>
         </div>
-        
-        <div class = "username" style={{ flex: 4}}>
-          <text style={styles.geral}>Hi, username</text>
+        <div className='center'>
+          <h2>LIST OF QUIZZES FOR REVIEW</h2>
+          <h2>Please click on the quiz that you would like review</h2>
         </div>
-        <div>
-          <text style = {styles.middletitle}>LIST OF QUIZZES TO BE REVIEWED</text>
+        <div className='center_tab'>
+            <ul>
+              {(() => {
+                var d = [];
+                for (let i = 0; i < data.length; i++) {
+                  if (i === 0 && data.length === 1) {
+                    d.push(
+                      <tr>
+                      <button className='button_uniq'>
+                        {data[i][0]}-
+                        {data[i][1]}-
+                        {data[i][2]}-
+                        {data[i][3]}-
+                        {data[i][4]}
+                      </button>
+                    </tr>)
+                  } else if (i === 0 && data.length > 0) {
+                    d.push(
+                      <tr>
+                      <button className='button1'>
+                      {data[i][0]}-
+                      {data[i][1]}-
+                      {data[i][2]}-
+                      {data[i][3]}-
+                      {data[i][4]}
+                      </button>
+                    </tr>)
+                  } else if (i > 0 && i !== data.length-1) {
+                    d.push(
+                      <tr>
+                      <button className='other_buttons'>
+                        {data[i][0]}-
+                        {data[i][1]}-
+                        {data[i][2]}-
+                        {data[i][3]}-
+                        {data[i][4]}
+                      </button>
+                    </tr>)
+                  } else if (i === data.length-1) {
+                    d.push(
+                      <tr>
+                      <button className='button_end'>
+                        {data[i][0]}-
+                        {data[i][1]}-
+                        {data[i][2]}-
+                        {data[i][3]}-
+                        {data[i][4]}
+                      </button>
+                    </tr>)
+                  }
+                }
+              return d;
+              })()}
+            </ul>
         </div>
       </div>
-</div>
-    );
-  }
-
-  const styles = {
-    geral: {
-      fontSize: 20,
-    },
-    middletitle:{
-      fontSize: 20,
-      fontWeight: "bold"
-    },
-  
+    )
   };
-  
-  
+
 export default ReviewQuizPage;
