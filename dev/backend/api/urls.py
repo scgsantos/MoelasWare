d663c5d14876c89d9fb219222e9bf9d88abe02a3
Pedro Ascensão <uc2020233012@student.uc.pt>
2022-11-07 20:45:24 +0000

    Added backend

--- dev/backend/api/urls.py
+++ dev/backend/api/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 	path('tests/<int:pk>/', views.get_test_view),
 	path('users/<int:pk>/submissions', views.submissions_by_user_view),
 	path('fame', views.hall_of_fame_view),
-	path('tests/<int:pk>/submissions', views.submission_of_a_test_view)
+	path('tests/<int:pk>/submissions', views.submission_of_a_test_view),
+	path('tests', views.get_all_tests)
 ]
 
