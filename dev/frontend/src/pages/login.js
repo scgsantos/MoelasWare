2056b4267c3410382bc76b43c9bd381f7df1e302
aritaoliveira <102245591+aritaoliveira@users.noreply.github.com>
2022-11-07 02:20:23 +0000

    Correcoes

--- dev/frontend/src/pages/login.js
+++ dev/frontend/src/pages/login.js
@@ -17,18 +17,18 @@ function Login() {
           <h2>
             LOGIN
           </h2>
-          <input id="username_log" type="username" placeholder='username'></input>
+          <input id="username_log" type="email" placeholder='username'></input>
           <input id="password_log" type="password" placeholder='password'></input>
-          <button type="enter_log" onclick="#login">ENTER</button>
+          <button type="submit" onclick="#login">ENTER</button>
         </div>
         <div className="register">
           <h2>
             REGISTER
           </h2>
-          <input id="username_reg" type="username" placeholder='username'></input>
+          <input id="username_reg" type="email" placeholder='username'></input>
           <input id="password_reg" type="password" placeholder='password'></input>
           <input id="repeat_password" type="password" placeholder='repeat password'></input>
-          <button type="enter_reg" onclick="#register">ENTER</button>
+          <button type="submit" onclick="#register">ENTER</button>
         </div>
       </div>
     </div>
