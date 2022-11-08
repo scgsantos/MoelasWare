22010008c32c067fbea1e89bd2e6806033c1f3ff
aritaoliveira <102245591+aritaoliveira@users.noreply.github.com>
2022-11-08 16:03:27 +0000

    butoes corretos

--- dev/frontend/src/pages/login.js
+++ dev/frontend/src/pages/login.js
@@ -19,7 +19,7 @@ function Login() {
           </h2>
           <input id="username_log" type="email" placeholder='username'></input>
           <input id="password_log" type="password" placeholder='password'></input>
-          <button type="submit" onclick="#login">ENTER</button>
+          <button className="btn btn_click" type="submit" onclick="#login">ENTER</button>
         </div>
         <div className="register">
           <h2>
@@ -28,7 +28,7 @@ function Login() {
           <input id="username_reg" type="email" placeholder='username'></input>
           <input id="password_reg" type="password" placeholder='password'></input>
           <input id="repeat_password" type="password" placeholder='repeat password'></input>
-          <button type="submit" onclick="#register">ENTER</button>
+          <button className="btn btn_click" type="submit" onclick="#register">ENTER</button>
         </div>
       </div>
     </div>
