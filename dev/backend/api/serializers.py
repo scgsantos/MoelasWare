bfa81ff42002bb0af07f53227b5939a435f4e447
Tomás Duarte <tomasduarte@pm.me>
2022-11-09 15:55:05 +0000

    Run pipeline on changes

--- dev/backend/api/serializers.py
+++ dev/backend/api/serializers.py
@@ -1,9 +1,8 @@
+from moelasware.models import Quiz, QuizAnswer, Tag, Test, User
 from rest_framework import serializers
 
-from moelasware.models import Test, Tag, Quiz, User, QuizAnswer
 
-
-class CreateTestSerializer( serializers.ModelSerializer ):
-	class Meta:
-		model = Test
-		fields = ['author', 'allowed_tags', 'quizzes', 'name', 'num_quizzes']
+class CreateTestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Test
+        fields = ["author", "allowed_tags", "quizzes", "name", "num_quizzes"]
--- dev/backend/api/views.py
+++ dev/backend/api/views.py
@@ -1,20 +1,20 @@
 from random import sample
 
-from django.http import JsonResponse, HttpResponseForbidden, HttpResponseBadRequest, Http404
-from rest_framework.decorators import api_view
+from api.serializers import (CreateTestSerializer, GetTestSerializer,
+                             QuizAnswerSerializer, QuizSerializer)
+from django.http import (Http404, HttpResponseBadRequest,
+                         HttpResponseForbidden, JsonResponse)
+from moelasware.models import Quiz, QuizAnswer, Tag, Test
 from rest_framework import status
+from rest_framework.decorators import api_view
 
-from moelasware.models import Test, Quiz, Tag, QuizAnswer
-from api.serializers import CreateTestSerializer, GetTestSerializer, QuizSerializer, QuizAnswerSerializer
-
-from moelasware.models import Quiz, Test
 from .serializers import GetTestSerializer
 
 DEFAULT_TEST_PAGE_LIMIT = 20
 DEFAULT_TAG_PAGE_LIMIT = 20
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 # @login_required
 def get_test_view(request, pk):
     # get the user object from the logged in user
@@ -22,18 +22,19 @@ def get_test_view(request, pk):
 
     # if the user doesnt exist return not logged in
     if not user.is_authenticated:
-        return HttpResponseForbidden('Not logged in')
+        return HttpResponseForbidden("Not logged in")
 
     # check if the uer is able to solve the test
     if not user.can_solve_tests():
         # You should just return a simple plain-text response, no need for JSON. Use HttpResponseForbidden.
-        return HttpResponseForbidden('You are not allowed to solve tests')
+        return HttpResponseForbidden("You are not allowed to solve tests")
 
     # get test by id -> detail view
     instance = get_object_or_404(Test, pk=pk)
     serializer = GetTestSerializer(instance, many=False)
 
-    return JsonResponse({'test': serializer.data}, status=status.HTTP_200_OK)
+    return JsonResponse({"test": serializer.data}, status=status.HTTP_200_OK)
+
 
 # @api_view(['POST'])
 # @login_required
@@ -43,7 +44,7 @@ def create_submission(request, pk, user):
     # TODO: Change answers from being an array of array of integers
     # to allow for questions being out of order
     # for example:
-    """  
+    """
     {
         "answers": [
             {
@@ -61,54 +62,56 @@ def create_submission(request, pk, user):
     # check if the uer is able to solve the test
     if not user.can_solve_tests():
         # You should just return a simple plain-text response, no need for JSON. Use HttpResponseForbidden.
-        return HttpResponseForbidden('You are not allowed to solve tests')
+        return HttpResponseForbidden("You are not allowed to solve tests")
 
     # get test by id -> detail view
     instance = get_object_or_404(Test, pk=pk)
 
     # get the answers from the request body
-    answers = request.data.get('answers', None)
+    answers = request.data.get("answers", None)
 
     # check if the answers are valid
     if not answers:
-        return HttpResponseBadRequest('No answers provided')
+        return HttpResponseBadRequest("No answers provided")
 
     if not isinstance(answers, list):
-        return HttpResponseBadRequest('Answers must be a list')
+        return HttpResponseBadRequest("Answers must be a list")
 
     # check if the answers are valid
     for answer in answers:
         if not isinstance(answer, dict):
-            return HttpResponseBadRequest('Answers must be a list of dicts')
+            return HttpResponseBadRequest("Answers must be a list of dicts")
 
-        if not answer.get('quiz_id', None):
-            return HttpResponseBadRequest('Answers must have a quiz_id')
+        if not answer.get("quiz_id", None):
+            return HttpResponseBadRequest("Answers must have a quiz_id")
 
-        if not answer.get('quiz_answers', None):
-            return HttpResponseBadRequest('Answers must have a quiz_answers')
+        if not answer.get("quiz_answers", None):
+            return HttpResponseBadRequest("Answers must have a quiz_answers")
 
-        if not isinstance(answer.get('quiz_answers', None), list):
-            return HttpResponseBadRequest('quiz_answers must be a list')
+        if not isinstance(answer.get("quiz_answers", None), list):
+            return HttpResponseBadRequest("quiz_answers must be a list")
 
     # check if the answers are valid
     for answer in answers:
-        quiz_id = answer.get('quiz_id', None)
-        quiz_answers = answer.get('quiz_answers', None)
+        quiz_id = answer.get("quiz_id", None)
+        quiz_answers = answer.get("quiz_answers", None)
 
         # get the quiz
         quiz = get_object_or_404(Quiz, pk=quiz_id)
 
         # check if the quiz is in the test
         if quiz not in instance.quizzes.all():
-            return HttpResponseBadRequest('Quiz is not in the test')
+            return HttpResponseBadRequest("Quiz is not in the test")
 
         # check if the quiz answers are valid
         for quiz_answer in quiz_answers:
             if not isinstance(quiz_answer, int):
-                return HttpResponseBadRequest('quiz_answers must be a list of integers')
+                return HttpResponseBadRequest("quiz_answers must be a list of integers")
 
             if quiz_answer < 0 or quiz_answer >= quiz.get_number_of_options():
-                return HttpResponseBadRequest('quiz_answers must be a list of integers between 0 and the number of options')
+                return HttpResponseBadRequest(
+                    "quiz_answers must be a list of integers between 0 and the number of options"
+                )
 
     # create the submission
     try:
@@ -118,7 +121,8 @@ def create_submission(request, pk, user):
             return HttpResponseBadRequest(e)
 
     # return the submission
-    return JsonResponse({'submission': submission}, status=status.HTTP_200_OK)
+    return JsonResponse({"submission": submission}, status=status.HTTP_200_OK)
+
 
 # @api_view(['GET'])
 # @login_required
@@ -128,13 +132,13 @@ def get_self_submission_view(request, pk, user):
     # check if the uer is able to solve the test
     if not user.can_solve_tests():
         # You should just return a simple plain-text response, no need for JSON. Use HttpResponseForbidden.
-        return HttpResponseForbidden('You are not allowed to solve tests')
+        return HttpResponseForbidden("You are not allowed to solve tests")
 
     # get test by id -> detail view
     instance = get_object_or_404(Test, pk=pk)
     serializer = GetTestSerializer(instance, many=False)
 
-    return JsonResponse({'test': serializer.data}, status=status.HTTP_200_OK)
+    return JsonResponse({"test": serializer.data}, status=status.HTTP_200_OK)
 
 
 @api_view(["GET", "POST"])
@@ -144,11 +148,10 @@ def submission_view(request, pk):
     user = request.user
 
     if not user.is_authenticated:
-        return HttpResponseForbidden('Not logged in')
-    
+        return HttpResponseForbidden("Not logged in")
+
     proxy = {
         "GET": get_self_submission_view,
         "POST": create_submission,
     }
     return proxy[request.method](request, pk, user)
-    
