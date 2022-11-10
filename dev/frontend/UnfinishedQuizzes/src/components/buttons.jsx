55fdf70260f3b8d2177be4b440670af3b089d0c1
Rodrigo Santos Figueiredo <rodres2002@gmail.com>
2022-11-10 23:13:26 +0000

    nada

--- dev/frontend/UnfinishedQuizzes/src/components/buttons.jsx
+++ dev/frontend/UnfinishedQuizzes/src/components/buttons.jsx
@@ -3,13 +3,17 @@ import "../CSS/index.css";
 
 class buttons extends Component {
 
+    state = {
+        link : this.props.linkzao
+    }
     render() { 
         return (
             <div class = 'unfinishedquiz'>
-                <h1 class = "toptitle">Quiz #<span>{this.props.buttonNumber}</span></h1>
-                <button onclick = "location.href = 'https://www.culturagenial.com/monumento-cristo-redentor/'" class = 'button'>Quiz #<span>{this.props.buttonNumber}</span></button>
+                <h1 class = "toptitle"> Quiz #<span>{this.props.buttonNumber}</span></h1>
+                <a class = "vski" href= "https://pbs.twimg.com/media/DtlG-4oWoAIKdd4.jpg">
+                    <button  class = 'button'  >Quiz #<span>{this.props.buttonNumber}</span></button>
+                </a>
             </div>
-            
         );
     }
 }
