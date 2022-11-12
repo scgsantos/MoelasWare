df0186b0ca508a1fa778e7cc6da7c075034a0c23
Tomás Duarte <git@tomasduarte.eu>
2022-11-12 20:32:57 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- dev/backend/moelasware/settings.py
+++ dev/backend/moelasware/settings.py
@@ -89,10 +89,7 @@ WSGI_APPLICATION = 'moelasware.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 DEFAULT_SQLITE_PATH = "db.sqlite3"
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=f"sqlite:///{BASE_DIR / DEFAULT_SQLITE_PATH}")
 }
 
 
