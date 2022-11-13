fcb5f6bc3a122a028dac0795c1dd617707fc71d1
LeomPina <leonardopina2001@gmail.com>
2022-11-13 23:04:16 +0000

    Serializers Fixed

--- dev/backend/api/serializers.py
+++ dev/backend/api/serializers.py
@@ -66,12 +66,12 @@ class HallOfFameGetTestInfo(serializers.ModelSerializer):
 	solved_tests = serializers.SerializerMethodField('get_solved_tests')
 	class Meta:
 		model = Test
-		fields = ['id','author','quizzes', 'solved_tests']
+		fields = ['id','author','quizzes','solved_tests']
 
 	def get_solved_tests(self, obj):
-		return SubmissionAnswer.objects.filter(submission__test__id=obj.id).count()
+		return Submission.objects.filter(test__id=obj.id).count()
 
-class HallOfFameSubmissionSerializer(serializers.ModelSerializer):
+class HallOfFameSubmissionSerializer(serializers.ModelSerializer): 
 	test = HallOfFameGetTestInfo(read_only=True)
 	class Meta:
 		model = Submission
@@ -83,3 +83,18 @@ class HallOfFame(serializers.ModelSerializer):
 	class Meta:
 		model = SubmissionAnswer
 		fields = ['submission']
+
+class HallOfFameGetUserInfo(serializers.ModelSerializer):
+	user = GetAuthUserAll(read_only=True)
+	correct_answers = serializers.SerializerMethodField('get_correct_answers')
+	solved_tests = serializers.SerializerMethodField('get_all_solved_tests')
+	class Meta:
+		model = User
+		fields = ['id', 'user', 'correct_answers', 'solved_tests']
+
+	def get_correct_answers(self, obj):
+		return SubmissionAnswer.objects.filter(submission__submitter=obj).filter(answer__correct=True).count()	
+
+	def get_all_solved_tests(self, obj):
+		return Submission.objects.filter(submitter__user__username=obj.user.username).count()	
+	
--- dev/backend/api/views.py
+++ dev/backend/api/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
 from moelasware.models import Test, Submission, SubmissionAnswer, User, AuthUser
-from .serializers import GetSubmissionsAnsweredByTest, GetTestSerializer, CreateTestSerializer, HallOfFame, HallOfFameGetTestInfo
+from .serializers import GetSubmissionsAnsweredByTest, GetTestSerializer, CreateTestSerializer, HallOfFame, HallOfFameGetTestInfo, HallOfFameGetUserInfo
 
 
 @api_view(['GET']) # allowed method(s)
@@ -69,31 +69,42 @@ def submissions_by_user_view(request, pk):
     return JsonResponse({'submissions' : submission})
 
 def return_date(date:str):
-	date_months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
-	x = date.replace(" ", "-").split("-")
-	date_string = date_months[int(x[1]) - 1] + " " + x[2] + " " + x[0]
-	return date_string
+    date_months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+    x = date.replace("T", "-").split("-")
+    date_string = date_months[int(x[1]) - 1] + " " + x[2] + " " + x[0]
+    return date_string
+
+def handle_serializer_hall_of_fame_view(obj):
+    info_list = []
+    print("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    
+    for i in obj:
+        print(i)
+        author = i["user"]["username"]
+        correct_answers = i["correct_answers"]
+        date_joined = return_date(str(i["user"]["date_joined"]))
+        solved_tests = i["solved_tests"]
+        info_list.append({i["id"]: [author, correct_answers, solved_tests, date_joined, i["id"], i["user"]["email"]]})
+    
+    return info_list 
 
 @api_view(['GET'])
 def hall_of_fame_view(request): # falta serializer
 
-	users = User.objects.all().order_by('user') 
-	if not users.exists():
-		return HttpResponseNotFound('User not found')
+    users = User.objects.all().order_by('user')
+    if not users.exists():
+        return HttpResponseNotFound('User not found')  
 
-	if not SubmissionAnswer.objects.all().exists():
-		return HttpResponseNotFound('Submissions not found')
+    if not SubmissionAnswer.objects.all().exists():
+        return HttpResponseNotFound('Submissions not found')
+    
+    sub = HallOfFameGetUserInfo(users, many=True).data
 
-	info_list = []
-	for user in users:
-		username = user.user.username
-		solved_tests = SubmissionAnswer.objects.filter(submission__submitter__user__username=user.user.username).count()
-		date_joined = return_date(str(user.user.date_joined))
-		correct_answers = SubmissionAnswer.objects.filter(submission__submitter=user).filter(answer__correct=True).count()
-		info_list.append({user.user.id: [username, date_joined, solved_tests, correct_answers, user.user.id, user.user.email]})
+    sub = handle_serializer_hall_of_fame_view(sub)
+    print(sub)#
 
     #TODO Create Serializer for this
-	return JsonResponse({'fame': info_list})
+    return JsonResponse({'fame': sub})
 
 def handle_serializer_test(obj):
     obj = handle_serializer(obj)
