4d84fa791da5c1f04c63d249f3617ccab663a425
Tomás Duarte <tomasduarte@pm.me>
2022-11-16 14:39:58 +0000

    Fix Docker setup
    Add netcat to Dockerfile (meh), for a wait-for script.
    Make the Compose file more robust, and fix the PostgreSQL password
    issue.
    With this, the API is fully deployed.
    
    Closes #439, #440

--- dev/backend/moelasware/settings.py
+++ dev/backend/moelasware/settings.py
@@ -36,7 +36,7 @@ def set_secret_key_and_debug():
 
 SECRET_KEY, DEBUG = set_secret_key_and_debug()
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["moelasware", "api.moelasware.xyz"]
 
 # TODO: Check if we really want to allow all origins
 CORS_ALLOW_ALL_ORIGINS = True
