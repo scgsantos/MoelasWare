a4c8d793901b4a5a9b3019460b26d5bc92fb0da9
Tomás Duarte <tomasduarte@pm.me>
2022-11-19 15:16:51 +0000

    Change config.svurl from string to URL

--- dev/frontend/src/config.js
+++ dev/frontend/src/config.js
@@ -1,5 +1,5 @@
-const API_DEV_URL = "http://localhost:8000";
-const API_PROD_URL = "https://api.moelasware.xyz";
+const API_DEV_URL = new URL("http://localhost:8000");
+const API_PROD_URL = new URL("https://api.moelasware.xyz");
 
 const isdev = () => !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
