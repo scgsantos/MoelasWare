52af963377b1d8b4ab1ec9715a8b34a8183e56ba
sofiaTrindade <sofiacaetanotrindade@gmail.com>
2022-11-20 16:20:02 +0000

    Change get number of quizzes to the api method and fix warnings

--- dev/frontend/src/components/InputNumber.jsx
+++ dev/frontend/src/components/InputNumber.jsx
@@ -1,52 +1,61 @@
 function IncDecCounter(props) {
-  const numberValidation = new RegExp("^([1-9][0-9]{0,2})$");
+    const numberValidation = new RegExp("^([1-9][0-9]{0,2})$");
 
-  let incNum = () => {
-    if (props.num < 999) {
-      props.setNum(Number(props.num) + 1);
-    }
-  };
+    let incNum = () => {
+        if (props.num < 999) {
+            props.setNum(Number(props.num) + 1);
+        }
+    };
 
-  let decNum = () => {
-    if (props.num > 1) {
-      props.setNum(props.num - 1);
-    }
-  };
+    let decNum = () => {
+        if (props.num > 1) {
+            props.setNum(props.num - 1);
+        }
+    };
 
-  let handleChange = (e) => {
-    if (numberValidation.test(e.target.value)) {
-      props.setNum(e.target.value);
-    } else {
-      props.setNum(1);
-    }
-  };
+    let handleChange = (e) => {
+        if (numberValidation.test(e.target.value)) {
+            props.setNum(e.target.value);
+        } else {
+            props.setNum(1);
+        }
+    };
 
-  return (
-    <>
-      <h1 className="inputNumberTitle">{props.label}</h1>
-      <div
-        class="inputNumber"
-        style={{ display: "flex", alignItems: "center" }}
-      >
-        <div class="addDiv">
-          <button className="addButton" type="button" onClick={decNum}>
-            -
-          </button>&nbsp;
-        </div>
-        <input
-          type="text"
-          class="formControl"
-          value={props.num}
-          onChange={handleChange}
-        />
-        <div class="subDiv">
-          <button class="subButton" type="button" onClick={incNum}>
-            +
-          </button>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <h1 className="inputNumberTitle">{props.label}</h1>
+            <div
+                className="inputNumber"
+                style={{ display: "flex", alignItems: "center" }}
+            >
+                <div className="addDiv">
+                    <button
+                        className="addButton"
+                        type="button"
+                        onClick={decNum}
+                    >
+                        -
+                    </button>
+                    &nbsp;
+                </div>
+                <input
+                    type="text"
+                    className="formControl"
+                    value={props.num}
+                    onChange={handleChange}
+                />
+                <div className="subDiv">
+                    <button
+                        className="subButton"
+                        type="button"
+                        onClick={incNum}
+                    >
+                        +
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default IncDecCounter;
--- dev/frontend/src/pages/create_test/number_only/NumberOnly.jsx
+++ dev/frontend/src/pages/create_test/number_only/NumberOnly.jsx
@@ -4,166 +4,183 @@ import "pages/create_test/number_only/NumberOnly.css";
 
 import { useNavigate } from "react-router-dom";
 
-import { CREATE_RANDOM_TEST_URL, TEST_MENU_URL, TEST_PREVIEW_URL } from "urls.js";
+import {
+    CREATE_RANDOM_TEST_URL,
+    TEST_MENU_URL,
+    TEST_PREVIEW_URL,
+} from "urls.js";
 import history from "history.js";
+import API from "api.js";
 
 function CreateRandomTest() {
-  document.body.style = "background: var(--pink)";
-  const [num, setNum] = useState(1);
-  const [isPage1, setIsPage1] = useState(true);
-  const [text, setText] = useState("");
+    document.body.style = "background: var(--pink)";
+    const [num, setNum] = useState(1);
+    const [isPage1, setIsPage1] = useState(true);
+    const [text, setText] = useState("");
 
-  const [quizzes, setQuizzes] = useState([]);
-  const [quizzes_count, setQuizzesCount] = useState(-1);
+    const [quizzes, setQuizzes] = useState([]);
+    const [quizzes_count, setQuizzesCount] = useState(-1);
 
-  if (quizzes_count == -1) {
-    getQuizzesCount();
-  }
-
-  console.log(history.location);
-
-  if (history.location.state != null && quizzes.length == 0) {
-    setNum(history.location.state.quizzes?.length);
-    setText(history.location.state.name);
-    setQuizzes(history.location.state?.quizzes);
-  }
-
-  let navigate = useNavigate();
-
-  function getFirstQuiz() {
-    fetch("http://localhost:8000/api/quizzes/gen/", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ num_quizzes: num }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setQuizzes(data.quizzes);
-      });
-  }
-
-  function getQuizzesCount() {
-    fetch("http://localhost:8000/api/quizzes/count/")
-      .then((response) => response.json())
-      .then((data) => {
-        setQuizzesCount(data.quizzes_count);
-      });
-  }
-
-  function handleCreateButtonChange() {
-    if (num <= quizzes_count) {
-      setIsPage1(false);
+    if (quizzes_count == -1) {
+        API.getNumQuizzes().then((response) => {
+            setQuizzesCount(response.quizzes_count);
+        });
     }
 
-    if (quizzes.length != num) {
-      getFirstQuiz();
+    console.log(history.location);
+
+    if (history.location.state != null && quizzes.length == 0) {
+        setNum(history.location.state.quizzes?.length);
+        setText(history.location.state.name);
+        setQuizzes(history.location.state?.quizzes);
     }
-  }
 
-  function handleNameChange(e) {
-    setText(e.target.value);
-  }
+    let navigate = useNavigate();
 
-  function handleNextButtonChange() {
-    if (text.length != 0) {
-      history.push(CREATE_RANDOM_TEST_URL);
-
-      navigate(TEST_PREVIEW_URL, {
-        state: {
-          name: text,
-          quizzes: quizzes,
-          previous_path: CREATE_RANDOM_TEST_URL,
-        },
-      });
-      window.location.reload();
+    function getFirstQuiz() {
+        fetch("http://localhost:8000/api/quizzes/gen/", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ num_quizzes: num }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setQuizzes(data.quizzes);
+            });
     }
-  }
 
-  function handleGoBackChange() {
-    setIsPage1(true);
-  }
+    function getQuizzesCount() {
+        fetch("http://localhost:8000/api/quizzes/count/")
+            .then((response) => response.json())
+            .then((data) => {
+                setQuizzesCount(data.quizzes_count);
+            });
+    }
 
-  function handleGoBackToMenu() {
-    navigate(TEST_MENU_URL);
-  }
+    function handleCreateButtonChange() {
+        if (num <= quizzes_count) {
+            setIsPage1(false);
+        }
 
-  if (isPage1) {
-    return (
-      <div className="NumberOnly-firstPage">
-        <h1 className="NumberOnly-title">Create a Test</h1>
-        <h2 className="NumberOnly-subTitle">Random Test</h2>
-        <div className="NumberOnly-formControl">
-          <IncDecCounter
-            num={num}
-            setNum={setNum}
-            label={"Choose the number of quizzes you want in your test"}
-          />
-        </div>
-        <h2 className="NumberOnly-errorQuizzesCounter">
-          {num > quizzes_count
-            ? "The number chosen is greater than the number of existing quizzes"
-            : ""}
-        </h2>
-        <div className="NumberOnly-Publish-GoBack-buttons">
-          <div className="NumberOnly-buttonCreate">
-            <button onClick={handleGoBackToMenu}>Go Back</button>
-          </div>
-          &ensp;&ensp;
-          <div className="NumberOnly-buttonCreate">
-            <button onClick={handleCreateButtonChange}>Next</button>
-          </div>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="NumberOnly-secondPage">
-        <h1 className="NumberOnly-title">Create a Test</h1>
-        <h2 className="NumberOnly-subTitle">Random Test</h2>
+        if (quizzes.length != num) {
+            getFirstQuiz();
+        }
+    }
 
-        <div className="NumberOnly-inputDiv">
-          <h1 className="NumberOnly-inputTitle">{"Name your test"}</h1>
-          <input
-            className="NumberOnly-inputText"
-            type="text"
-            name="name"
-            value={text}
-            onChange={handleNameChange}
-          />
-          <h2 className="NumberOnly-errorInput">
-            {text.length > 0 ? "" : "Name is mandatory"}
-          </h2>
-        </div>
+    function handleNameChange(e) {
+        setText(e.target.value);
+    }
 
-        <h3 className="NumberOnly-title"> Chosen Quizzes: </h3>
-        <ul className="NumberOnly-quizList">
-          {quizzes.map((quiz) => (
-            <li className="NumberOnly-quiz">{quiz.question}</li>
-          ))}
-        </ul>
+    function handleNextButtonChange() {
+        if (text.length != 0) {
+            history.push(CREATE_RANDOM_TEST_URL);
 
-        <div className="NumberOnly-Publish-GoBack-buttons">
-          <button className="NumberOnly-GoBackbutton" onClick={handleGoBackChange}>
-            Go Back
-          </button>
+            navigate(TEST_PREVIEW_URL, {
+                state: {
+                    name: text,
+                    quizzes: quizzes,
+                    previous_path: CREATE_RANDOM_TEST_URL,
+                },
+            });
+            window.location.reload();
+        }
+    }
 
-          <button className="NumberOnly-GoBackbutton" onClick={getFirstQuiz}>
-            Reroll Quizzes
-          </button>
+    function handleGoBackChange() {
+        setIsPage1(true);
+    }
 
-          <button
-            className="NumberOnly-NextButton"
-            onClick={handleNextButtonChange}
-          >
-            Next
-          </button>
-        </div>
-      </div>
-    );
-  }
+    function handleGoBackToMenu() {
+        navigate(TEST_MENU_URL);
+    }
+
+    if (isPage1) {
+        return (
+            <div className="NumberOnly-firstPage">
+                <h1 className="NumberOnly-title">Create a Test</h1>
+                <h2 className="NumberOnly-subTitle">Random Test</h2>
+                <div className="NumberOnly-formControl">
+                    <IncDecCounter
+                        num={num}
+                        setNum={setNum}
+                        label={
+                            "Choose the number of quizzes you want in your test"
+                        }
+                    />
+                </div>
+                <h2 className="NumberOnly-errorQuizzesCounter">
+                    {num > quizzes_count
+                        ? "The number chosen is greater than the number of existing quizzes"
+                        : ""}
+                </h2>
+                <div className="NumberOnly-Publish-GoBack-buttons">
+                    <div className="NumberOnly-buttonCreate">
+                        <button onClick={handleGoBackToMenu}>Go Back</button>
+                    </div>
+                    &ensp;&ensp;
+                    <div className="NumberOnly-buttonCreate">
+                        <button onClick={handleCreateButtonChange}>Next</button>
+                    </div>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="NumberOnly-secondPage">
+                <h1 className="NumberOnly-title">Create a Test</h1>
+                <h2 className="NumberOnly-subTitle">Random Test</h2>
+
+                <div className="NumberOnly-inputDiv">
+                    <h1 className="NumberOnly-inputTitle">
+                        {"Name your test"}
+                    </h1>
+                    <input
+                        className="NumberOnly-inputText"
+                        type="text"
+                        name="name"
+                        value={text}
+                        onChange={handleNameChange}
+                    />
+                    <h2 className="NumberOnly-errorInput">
+                        {text.length > 0 ? "" : "Name is mandatory"}
+                    </h2>
+                </div>
+
+                <h3 className="NumberOnly-title"> Chosen Quizzes: </h3>
+                <ul className="NumberOnly-quizList">
+                    {quizzes.map((quiz) => (
+                        <li className="NumberOnly-quiz">{quiz.question}</li>
+                    ))}
+                </ul>
+
+                <div className="NumberOnly-Publish-GoBack-buttons">
+                    <button
+                        className="NumberOnly-GoBackbutton"
+                        onClick={handleGoBackChange}
+                    >
+                        Go Back
+                    </button>
+
+                    <button
+                        className="NumberOnly-GoBackbutton"
+                        onClick={getFirstQuiz}
+                    >
+                        Reroll Quizzes
+                    </button>
+
+                    <button
+                        className="NumberOnly-NextButton"
+                        onClick={handleNextButtonChange}
+                    >
+                        Next
+                    </button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default CreateRandomTest;
