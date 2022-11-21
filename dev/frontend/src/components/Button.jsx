d279817492e935c920dddb5ddf9a33e392fd3590
Pedro Ascensão <uc2020233012@student.uc.pt>
2022-11-21 14:26:36 +0000

    Fix

--- dev/frontend/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Button(props) {
-  return (
-    <button className={`box-button ${props.disabled === true ? 'disabled' : ''}`} onClick={props.onClick}>{props.name}</button>
-  )
-}
-
-export default Button
