e8a97bffe1a46097b3fa0332c9e3fd2720b395b0
João Pedro Leite <gamingbound@gmail.com>
2022-11-23 23:11:56 +0000

    Include JWT in Authorization header on api.js

--- dev/frontend/src/api.js
+++ dev/frontend/src/api.js
@@ -1,203 +1,219 @@
 import config from "config.js";
 
+// const config = { svurl: "https://api.moelasware.xyz/" };
+
 const ACCEPT_JSON = {
-    Accept: "application/json",
-    "Content-Type": "application/json",
+  "Accept": "application/json",
+  "Content-Type": "application/json",
 };
 
 class API {
-    static makeRequest(
-        path,
-        method = "GET",
-        body = undefined,
-        params = undefined,
-        headers = undefined
-    ) {
-        const searchParams = new URLSearchParams(params);
+  static getBearerToken() {
 
-        // TODO: change url to URL()
-
-        return fetch(config.svurl + path + searchParams.toString(), {
-            method: method,
-            headers: { ...headers, ...ACCEPT_JSON },
-            body: method == "GET" ? undefined : JSON.stringify(body),
-        }).then((response) => response.json());
+    return sessionStorage.access === undefined ? {} : {
+      "Authorization": "Bearer " + sessionStorage.access,
     }
+  }
 
-    // Get test by ID
-    static getTest(test_id) {
-        return this.makeRequest("api/tests/", test_id.toString() + "/");
+  static makeRequest(
+    path,
+    method = "GET",
+    body = undefined,
+    params = undefined,
+    headers = undefined
+  ) {
+    const searchParams = new URLSearchParams(params);
+    const pathURL = new URL(path, config.svurl);
+    searchParams.forEach((value, key) => pathURL.searchParams.set(key, value));
+
+    return fetch(pathURL, {
+      method: method,
+      headers: { ...headers, ...ACCEPT_JSON, ...API.getBearerToken() },
+      body: method == "GET" ? undefined : JSON.stringify(body),
+    }).then((response) => response.json());
+  }
+
+  // Get test by ID
+  static getTest(test_id) {
+    return this.makeRequest("api/tests/", test_id.toString() + "/");
+  }
+
+  // Get all submissions of a test
+  static getTestSubmissions(test_id) {
+    return this.makeRequest(
+      "api/tests/",
+      test_id.toString() + "/submissions/"
+    );
+  }
+
+  // Get all tests
+  static getTests() {
+    return this.makeRequest("api/tests/");
+  }
+
+  // Get a tag by ID
+  static getTag(tag_id) {
+    return this.makeRequest("api/tags/", tag_id.toString() + "/");
+  }
+
+  // Get all tags
+  static getTags() {
+    return this.makeRequest("api/tags/");
+  }
+
+  // Get num_quizzes quizzes with tags in tags list (no tags ==> any tags allowed)
+  static genQuizzes(num_quizzes, tags) {
+    return this.makeRequest("api/quizzes/gen/", "POST", {
+      num_quizzes: num_quizzes,
+      tags: tags,
+    });
+  }
+
+  // Get all possible answers for a given quiz
+  static getQuizAnswers(quiz_id) {
+    return this.makeRequest(
+      "api/quizzes/",
+      quiz_id.toString() + "/answers/"
+    );
+  }
+
+  // Get number of quizzes in db
+  static getNumQuizzes() {
+    return this.makeRequest("api/quizzes/count/");
+  }
+
+  // Get a quiz by ID
+  static getQuiz(quiz_id) {
+    return this.makeRequest("api/quizzes/" + quiz_id.toString() + "/");
+  }
+
+  // NOT YET IMPLEMENTED
+  // Get all quizzes
+  static getQuizzes() {
+    return this.makeRequest("api/quizzes");
+  }
+
+  // Get all reviewers of a given quiz
+  static getQuizReviewers(quiz_id) {
+    return this.makeRequest(
+      "api/quizzes/" + quiz_id.toString() + "/reviewers/"
+    );
+  }
+
+  // NOT WORKING IN BACKEND
+  // Create a quiz review
+  static createQuizReview(quiz, reviewer, accepted, comment) {
+    return this.makeRequest("quizzes/review/", "POST", {
+      quiz: quiz,
+      reviewer: reviewer,
+      accepted: accepted,
+      comment: comment,
+    });
+  }
+
+  // Get all test submissions made by a given user
+  static getUserSubmissions(user_id) {
+    return this.makeRequest(
+      "api/users/" + user_id.toString() + "/submissions/"
+    );
+  }
+
+  // Get hall of fame
+  static getHallOfFame() {
+    return this.makeRequest("api/fame/");
+  }
+
+  // Get user login token
+  static login(username, password) {
+    let tokens = this.makeRequest("api/token/", "POST", {
+      username: username,
+      password: password,
+    });
+
+    tokens.then((data) => {
+      sessionStorage.setItem("access", data.access);
+      sessionStorage.setItem("refresh", data.refresh);
     }
+    );
+    return tokens;
+  }
 
-    // Get all submissions of a test
-    static getTestSubmissions(test_id) {
-        return this.makeRequest(
-            "api/tests/",
-            test_id.toString() + "/submissions/"
-        );
-    }
+  // Refresh token
+  static tokenRefresh(refresh) {
+    return this.makeRequest("api/token/refresh/", "POST", {
+      refresh: refresh,
+    });
+  }
 
-    // Get all tests
-    static getTests() {
-        return this.makeRequest("api/tests/");
-    }
+  // THE FOLLOWING ENDPOINTS ARE NOT IMPLEMENTED IN BACKEND
 
-    // Get a tag by ID
-    static getTag(tag_id) {
-        return this.makeRequest("api/tags/", tag_id.toString() + "/");
-    }
+  // Add revision
+  static addRevision(quiz_id) {
+    return this.makeRequest(
+      "api/quizzes/" + quiz_id.toString() + "/",
+      "PATCH"
+    );
+  }
 
-    // Get all tags
-    static getTags() {
-        return this.makeRequest("api/tags/");
-    }
+  // Post a quiz
+  static postQuiz(question, accepted, answers) {
+    return this.makeRequest("api/quizzes/", "POST", {
+      question: question,
+      accepted: accepted,
+      answers: answers,
+    });
+  }
 
-    // Get num_quizzes quizzes with tags in tags list (no tags ==> any tags allowed)
-    static getNumberOfQuizzes(num_quizzes, tags) {
-        return this.makeRequest("api/quizzes/gen/", "POST", {
-            num_quizzes: num_quizzes,
-            tags: tags,
-        });
-    }
+  // Get rejected reviews
+  static getRejectedReviews(quiz_id) {
+    return this.makeRequest(
+      "api/quizzes/" + quiz_id.toString() + "/revisions/"
+    );
+  }
 
-    // Get all possible answers for a given quiz
-    static getQuizAnswers(quiz_id) {
-        return this.makeRequest(
-            "api/quizzes/",
-            quiz_id.toString() + "/answers/"
-        );
-    }
+  // Get a quizz's reviews
+  static getQuizzReview(quiz_id) {
+    return this.makeRequest(
+      "api/quizzes/" + quiz_id.toString() + "/reviews/"
+    );
+  }
 
-    // Get number of quizzes in db
-    static getNumQuizzes() {
-        return this.makeRequest("api/quizzes/count/");
-    }
+  // Post a review
+  static postQuizzReview(quiz_id) {
+    return this.makeRequest(
+      "api/quizzes/" + quiz_id.toString() + "/reviews/",
+      "POST"
+    );
+  }
 
-    // Get a quiz by ID
-    static getQuiz(quiz_id) {
-        return this.makeRequest("api/quizzes/" + quiz_id.toString() + "/");
-    }
+  // Generate a new test
+  static postTest(num_quizzes, allowed_tags, quizzes) {
+    return this.makeRequest("api/tests/", "POST", {
+      num_quizzes: num_quizzes,
+      allowed_tags: allowed_tags,
+      quizzes: quizzes,
+    });
+  }
 
-    // NOT YET IMPLEMENTED
-    // Get all quizzes
-    static getQuizzes() {
-        return this.makeRequest("api/quizzes");
-    }
+  // Get all submissions of a given test
+  static getAllTestSubmissions(test_id) {
+    return this.makeRequest(
+      "api/tests/" + test_id.toString() + "/submissions/"
+    );
+  }
 
-    // Get all reviewers of a given quiz
-    static getQuizReviewers(quiz_id) {
-        return this.makeRequest(
-            "api/quizzes/" + quiz_id.toString() + "/reviewers/"
-        );
-    }
+  // Add new submission
+  static postSubmission(test_id,) {
+    return this.makeRequest(
+      "api/tests/" + test_id.toString + "submissions",
+      "POST"
+    );
+  }
 
-    // NOT WORKING IN BACKEND
-    // Create a quiz review
-    static createQuizReview(quiz, reviewer, accepted, comment) {
-        return this.makeRequest("quizzes/review/", "POST", {
-            quiz: quiz,
-            reviewer: reviewer,
-            accepted: accepted,
-            comment: comment,
-        });
-    }
-
-    // Get all test submissions made by a given user
-    static getUserSubmissions(user_id) {
-        return this.makeRequest(
-            "api/users/" + user_id.toString() + "/submissions/"
-        );
-    }
-
-    // Get hall of fame
-    static getHallOfFame() {
-        return this.makeRequest("api/fame/");
-    }
-
-    // Get user login token
-    static login(username, password) {
-        return this.makeRequest("api/token/", "POST", {
-            username: username,
-            password: password,
-        });
-    }
-
-    // Refresh token
-    static tokenRefresh(refresh) {
-        return this.makeRequest("api/token/refresh/", "POST", {
-            refresh: refresh,
-        });
-    }
-
-    // THE FOLLOWING ENDPOINTS ARE NOT IMPLEMENTED IN BACKEND
-
-    // Add revision
-    static addRevision(quiz_id) {
-        return this.makeRequest(
-            "api/quizzes/" + quiz_id.toString() + "/",
-            "PATCH"
-        );
-    }
-
-    // Post a quiz
-    static postQuiz(question, accepted, answers) {
-        return this.makeRequest("api/quizzes/", "POST", {
-            question: question,
-            accepted: accepted,
-            answers: answers,
-        });
-    }
-
-    // Get rejected reviews
-    static getRejectedReviews(quiz_id) {
-        return this.makeRequest(
-            "api/quizzes/" + quiz_id.toString() + "/revisions/"
-        );
-    }
-
-    // Get a quizz's reviews
-    static getQuizzReview(quiz_id) {
-        return this.makeRequest(
-            "api/quizzes/" + quiz_id.toString() + "/reviews/"
-        );
-    }
-
-    // Post a review
-    static postQuizzReview(quiz_id) {
-        return this.makeRequest(
-            "api/quizzes/" + quiz_id.toString() + "/reviews/",
-            "POST"
-        );
-    }
-
-    // Generate a new test
-    static postTest(num_quizzes, allowed_tags, quizzes) {
-        return this.makeRequest("api/tests/", "POST", {
-            num_quizzes: num_quizzes,
-            allowed_tags: allowed_tags,
-            quizzes: quizzes,
-        });
-    }
-
-    // Get all submissions of a given test
-    static getAllTestSubmissions(test_id) {
-        return this.makeRequest(
-            "api/tests/" + test_id.toString() + "/submissions/"
-        );
-    }
-
-    // Add new submission
-    static postSubmission(test_id, ) {
-        return this.makeRequest(
-            "api/tests/" + test_id.toString + "submissions",
-            "POST"
-        );
-    }
-
-    // Get user with a given id
-    static getUser(user_id) {
-        return this.makeRequest("api/users/" + user_id.toString() + "/");
-    }
+  // Get user with a given id
+  static getUser(user_id) {
+    return this.makeRequest("api/users/" + user_id.toString() + "/");
+  }
 }
 
 export default API;
