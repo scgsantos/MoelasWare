b75e5c4e994acc8266d90281ec11368797b8befd
Polpedu <emailparaoedu@gmail.com>
2022-11-24 17:30:05 +0000

    code cleanup, search bar

--- dev/frontend/src/components/Button.jsx
+++ dev/frontend/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button(props) {
   return (
-    <button className={`box-button ${props.disabled === true ? 'disabled' : ''}`} onClick={props.onClick}>{props.name}</button>
+    <button className={`TestSelection-box-button ${props.disabled === true ? 'disabled' : ''}`} onClick={props.onClick}>{props.name}</button>
   )
 }
 
